c7c0bc0ccec7453fc21175922cc262cd61dec0a6
Ifeanyi Chidoka <ik.chidoka@gmail.com>
2024-06-08 10:48:03 +0100

    Added new file change update from main

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello Git");
+// console.log("Hello Git");
+
+const express = require('express')
+const PORT = 8000
+const HOST = 'http://localhost'
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Welcome to NodeJs")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on: ${HOST}:${PORT}`)
+})
