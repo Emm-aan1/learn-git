fd60140766e4c9020beec680461e8ef3a1f33f3e
Ifeanyi Chidoka <ik.chidoka@gmail.com>
2024-06-11 20:32:57 +0100

    removed git file from previous branch

--- Git/FE-BE/desktop/main.py
+++
@@ -1,4 +0,0 @@
-fe_be = "Frontend & Backend"
-cohort = "SE"
-
-print(f"I am learning {fe_be} from ALX cohort {cohort}")
--- Git/FE-BE/mobile/main.py
+++
@@ -1,6 +0,0 @@
-mobile = "Hello, I'm mobile version"
-adr = "Android"
-ios = "iOS"
-
-print(f"{mobile} - {adr}")
-print(f"{mobile} - {ios}")
